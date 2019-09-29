32cfcb3cc18cf1e299421ce0292a10aba11a7a5a
Yak Jun Xiang <snowypowers@gmail.com>
2019-09-28 17:03:55 -0700

    change rpc port

--- packages/neon-core/__integration__/rpc/privnet.ts
+++ packages/neon-core/__integration__/rpc/privnet.ts
@@ -2,7 +2,7 @@ import { rpc } from "../../src/index";
 
 let client: rpc.RPCClient;
 beforeAll(async () => {
-  client = new rpc.RPCClient("http://localhost:20333");
+  client = new rpc.RPCClient("http://localhost:30333");
 });
 
 describe("privnet", () => {
